4b79c6167ba5918f6077fe7254760c59010e65dc
cheehoong <lechho@gmail.com>
2024-02-07 16:54:23 +0800

    Update pre-install.sh

--- twingate/CONTROL/pre-install.sh
+++ twingate/CONTROL/pre-install.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
-APKG_PKG_DIR=/usr/local/AppCentral/sample-custom-apk
+APKG_PKG_DIR=/usr/local/AppCentral/twingate-docker
+TWINGATE_FOLDER=/share/Docker/$APKG_PKG_NAME
+LOGGING=$TWINGATE_FOLDER/log.txt
+printf "---pre-install---\n" >> $LOGGING
+
+# Make sure configuration folder exists
+if [ ! -d "$TWINGATE_FOLDER" ]; then
+	mkdir "$TWINGATE_FOLDER"
+fi
+
+docker pull twingate/connector:latest
+printf "Completed docker pull\n" >> $LOGGING
 
 case "$APKG_PKG_STATUS" in
 
 	install)
-		# pre install script here
+		# post install script here
+  		printf "pre-install\n" >> $LOGGING
 		;;
 	upgrade)
-		# pre upgrade script here (backup data)
-		# cp -af $APKG_PKG_DIR/etc/* $APKG_TEMP_DIR/.
+		# post upgrade script here (restore data)
+		# cp -af $APKG_TEMP_DIR/* $APKG_PKG_DIR/etc/.
+  		printf "pre-upgrade\n" >> $LOGGING
 		;;
 	*)
 		;;
 
 esac
+
+printf "pre-install end\n\n" >> $LOGGING
 
 exit 0
